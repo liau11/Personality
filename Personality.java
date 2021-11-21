1d029512f60d35989638ea6976805f9d438b0471
liau11 <aulily09@gmail.com>
2021-11-21 12:49:44 -0800

    added method to calculate B% and method to convert to personality type

--- Personality.java
+++ Personality.java
@@ -21,9 +21,9 @@ public class Personality {
             int[] bArr = new int[4]; 
             int [] aArr = new int [4];
             countSingleUser(name, testResponse, aArr, bArr);
+            int[] bPercent= bPercentage(name, aArr, bArr);
+            personality(bPercent);
         }
-
-
 
     }
 
@@ -40,34 +40,53 @@ public class Personality {
         for (int i = 0; i < testResponse.length(); i++) {
             char response = testResponse.charAt(i);
             if (i % 7 == 0){
-                if (response == 'B') {
+                if (response == 'B'|| response == 'b') {
                     bArr[0]++;
-                } else if (response == 'A') {
+                } else if (response == 'A'|| response == 'a') {
                     aArr[0]++; 
                 } 
             } else if (i % 7 == 1 || i % 7 == 2){
-                if (response == 'B') {
+                if (response == 'B'|| response == 'b') {
                     bArr[1]++;
-                } else if (response == 'A') {
+                } else if (response == 'A'|| response == 'a') {
                     aArr[1]++; 
                 }
             } else if (i % 7 == 3 || i % 7 == 4){
-                if (response == 'B') {
+                if (response == 'B'|| response == 'b') {
                     bArr[2]++;
-                } else if (response == 'A') {
+                } else if (response == 'A'|| response == 'a') {
                     aArr[2]++; 
                 }
             } else if (i % 7 == 5 || i % 7 == 6){
-                if (response == 'B') {
+                if (response == 'B'|| response == 'b') {
                     bArr[3]++;
-                } else if (response == 'A') {
+                } else if (response == 'A'|| response == 'a') {
                     aArr[3]++; 
                 }
             }
         }
-        System.out.print(name);
-        System.out.print(Arrays.toString(aArr));
-        System.out.print(Arrays.toString(bArr));
-        System.out.println();
+    }
+
+    public static int[] bPercentage(String name, int [] aArr, int [] bArr){
+        int [] bPercent = new int [4];
+        for (int i = 0; i < bArr.length; i++) {
+            bPercent[i] = (100 * bArr[i])/(bArr[i] + aArr[i]);
+        }
+        return bPercent;
+    }
+
+    public static void personality(int [] bPercent) {
+        char [] left = {'E', 'S', 'T', 'J'};
+        char [] right = {'I', 'I', 'F', 'P'};
+        char [] personality = new char [4];
+        for (int i = 0; i < bPercent.length; i++){
+            if (bPercent[i] > 50){
+                personality[i] = right[i];
+            } else if (bPercent[i] < 50) {
+                personality[i] = left[i];
+            } else {
+                personality[i] = 'X';
+            }
+        }
     }
 }
